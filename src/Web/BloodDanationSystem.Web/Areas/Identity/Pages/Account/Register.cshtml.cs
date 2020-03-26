e67127812c2b8b84501f2040fa0ff669c70dff90
Rosmari Nikova <rosmari.nikova@yahoo.com>
2020-03-26 19:31:36 +0200

    Add userRole to _Layout

--- src/Web/BloodDanationSystem.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ src/Web/BloodDanationSystem.Web/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -112,16 +112,16 @@
                     string htmlMessage = $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.";
                     await this.emailSender.SendEmailAsync(this.Input.Email, "Confirm your email", htmlMessage);
 
-                    if (this.userManager.Options.SignIn.RequireConfirmedAccount)
-                    {
-                        return this.RedirectToPage("RegisterConfirmation", new { email = this.Input.Email });
-                    }
-                    else
-                    {
+                    //if (this.userManager.Options.SignIn.RequireConfirmedAccount)
+                    //{
+                    //    return this.RedirectToPage("RegisterConfirmation", new { email = this.Input.Email });
+                    //}
+                    //else
+                    //{
                         await this.userManager.AddToRoleAsync(user, GlobalConstants.UserRoleName);
                         await this.signInManager.SignInAsync(user, isPersistent: false);
                         return this.LocalRedirect(returnUrl);
-                    }
+                    //}
                 }
 
                 foreach (var error in result.Errors)
