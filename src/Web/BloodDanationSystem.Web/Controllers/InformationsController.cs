82bdb8152a09276a4d435d4073486cfa1f90a147
Rosmari Nikova <rosmari.nikova@yahoo.com>
2020-04-01 01:13:12 +0300

    Add Article

--- src/Web/BloodDanationSystem.Web/Controllers/InformationsController.cs
+++ src/Web/BloodDanationSystem.Web/Controllers/InformationsController.cs
@@ -3,6 +3,7 @@
     using System.Threading.Tasks;
 
     using Aspose.Words;
+    using Aspose.Words.Saving;
     using BloodDanationSystem.Services;
     using BloodDonationSystem.Services.Models.Informations;
     using BloodDonationSystem.Web.InputModels.Informations;
@@ -54,9 +55,16 @@
         {
             string filepath = @"Articles/QA.docx";
             Document doc = new Document(filepath);
-            string allText = doc.ToString(SaveFormat.Html);
 
-            var text = allText.Substring(51062);
+            HtmlSaveOptions saveOptions = new HtmlSaveOptions
+            {
+                ExportHeadersFootersMode = ExportHeadersFootersMode.None,
+                ExportRelativeFontSize = true,
+                CssStyleSheetType = CssStyleSheetType.External,
+            };
+
+            string allText = doc.ToString(saveOptions);
+            var text = allText.Substring(406);
 
             this.ViewBag.WordHtml = text;
             return this.View();
